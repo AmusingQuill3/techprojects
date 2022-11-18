638546e637fac793b8ff37fb8f16993a7e5485f2
AmusingQuill3 <elijah.bixby@aol.com>
2022-11-18 16:32:53 -0500

    Create Vehicle.h

---
+++ School Projects/Sping '22/CSIS 112/Class Composition Assignment/Vehicle.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Dealership.h"
+
+class Vehicle
+{
+public:
+	
+	void listVehiclesMake(std::string Make);
+
+
+	void listVehiclesModel(std::string Model);
+
+
+	void listVehiclesYear(std::string Year);
+
+private:
+
+};
